fb09aeb472dd56cfb3ac3582f7d764f194ba47c2
unknown <David@.(none)>
2010-02-18 18:32:21 -0500

    first commit

---
+++ Space Cats V1.2/Managers/ScrollingBackgroundManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using System.Text;
+
+namespace Space_Cats_V1._2
+{
+    class ScrollingBackgroundManager
+    {
+        //Instance Variables
+        private ScrollingBackground z_backgroundImage1;
+        private ScrollingBackground z_backgroundImage2;
+        private Rectangle z_viewPort;
+
+        //Constructor
+        public ScrollingBackgroundManager(Texture2D background, Rectangle viewPort)
+        {
+            this.z_viewPort = viewPort;
+            this.z_backgroundImage1 = new ScrollingBackground(background);
+            this.z_backgroundImage2 = new ScrollingBackground(background);
+        }
+
+        //Update Method
+        public void Update()
+        {
+            //Update the Scrolling Background Images
+            if (this.z_backgroundImage1.getPosition().Y >= this.z_viewPort.Height)
+            {
+                //Then it is off of the stage, reset it back at the top
+                this.z_backgroundImage1.setPosition(new Vector2(0, 0.0f - this.z_viewPort.Height));
+                //this.z_backgroundImage1.upDatePosition();
+            }
+            if (this.z_backgroundImage2.getPosition().Y >= this.z_viewPort.Height)
+            {
+                //Then it is off of the stage, reset it back at the top
+                this.z_backgroundImage2.setPosition(new Vector2(0, 0.0f - this.z_viewPort.Height));
+                //this.z_backgroundImage2.upDatePosition();
+            }
+            //The order of the upDatePosition Matters I think
+            if (this.z_backgroundImage1.getPosition().Y > this.z_backgroundImage2.getPosition().Y)
+            {
+                this.z_backgroundImage2.upDatePosition();
+                this.z_backgroundImage1.upDatePosition();
+            }
+            else
+            {
+                this.z_backgroundImage1.upDatePosition();
+                this.z_backgroundImage2.upDatePosition();
+            }
+        }
+
+        //Draw Method
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //Draw Background images
+            if (this.z_backgroundImage1.getIsAlive())
+                spriteBatch.Draw(this.z_backgroundImage1.getSprite(), this.z_backgroundImage1.getPosition(), null,
+                    Color.White, 0, new Vector2(0, 0), this.z_backgroundImage1.Scale(this.z_viewPort)
+                    , SpriteEffects.None, 1);
+            if (this.z_backgroundImage2.getIsAlive())
+                spriteBatch.Draw(this.z_backgroundImage2.getSprite(), this.z_backgroundImage2.getPosition(), null,
+                    Color.White, 0, new Vector2(0, 0), this.z_backgroundImage2.Scale(this.z_viewPort)
+                    , SpriteEffects.None, 1);
+
+
+        }
+        
+    }
+}
---
+++ Space Cats V1.2/Objects/Enemy1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using System.Text;
+
+namespace Space_Cats_V1._2
+{
+    class Enemy1 : IEnemyShip
+    {
+        //Instance Variables
+        private AI_ZigZag z_AI;
+        private bool z_isAvailable;
+
+        //Constructor
+        public Enemy1(Texture2D loadedSprite, Rectangle viewPort)
+            : base(loadedSprite)
+        {
+            z_AI = new AI_ZigZag(viewPort);
+            this.setPosition(this.z_AI.getStartingPosition());
+            this.z_isAvailable = true;
+        }
+
+        //Accessors
+
+        //Mutators
+
+
+
+
+
+
+
+
+
+
+
+        public override void AIUpdate(GameTime gameTime)
+        {
+            if (this.z_AI.okToRemove())
+            {
+                this.setIsAlive(false);
+                return;
+            }
+
+            this.setVelocity(this.z_AI.calculateNewVelocity(this.getPosition(), gameTime));
+
+            this.upDatePosition();
+        }
+
+        public override bool getIsAvailable()
+        {
+            return this.z_isAvailable;
+        }
+
+        public override void setIsAvailable(bool isAvailable)
+        {
+            this.z_isAvailable = isAvailable;
+        }
+
+    }
+}
